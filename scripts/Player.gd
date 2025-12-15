0a07cda78c3cadba8fcc14f9826a3398aa5842ba
mrbartchak <matthewbartchak@gmail.com>
2025-12-14 22:48:19 -0500

    color switch bare bones functionality

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -2,20 +2,25 @@ extends Sprite2D
 
 @export var color_modes: Array[ColorMode]
 var current_mode: ColorMode
+var current_mode_idx: int
 #refactor later
-var movement_speed: float = 10.5
+var movement_speed: float = 2.5
 var is_firing: bool = false
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
-	current_mode = color_modes.pick_random()
+	set_color_mode(0)
 	self.texture = current_mode.texture
 	$AttackTimer.wait_time = 1.0 / current_mode.base_attack.fire_rate
 	$AttackTimer.start()
 
 func _physics_process(delta):
-	global_position = global_position.lerp(get_viewport().get_mouse_position(), movement_speed * delta)
+	_use_mouse_based_movement(delta)
 	_handle_shadow()
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey and event.pressed and event.keycode == KEY_SPACE:
+		set_color_mode((current_mode_idx + 1) % 3)
 
 #---Attacking---
 func _use_attack() -> void:
@@ -23,6 +28,17 @@ func _use_attack() -> void:
 
 func _on_attack_timer_timeout() -> void:
 	_use_attack()
+
+#---Color Switching---
+func set_color_mode(mode_idx: int) -> void:
+	current_mode_idx = mode_idx
+	current_mode = color_modes.get(mode_idx)
+	self.texture = current_mode.texture
+	$AttackTimer.wait_time = 1.0 / current_mode.base_attack.fire_rate
+
+#---Movement---
+func _use_mouse_based_movement(delta: float) -> void:
+	global_position = global_position.lerp(get_viewport().get_mouse_position(), movement_speed * delta)
 
 #---Visuals---
 func _handle_shadow() -> void:
